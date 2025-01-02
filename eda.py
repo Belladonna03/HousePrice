88973e65f3c45d83737da64dfd7ac701087921e4
kovaleva_darina <de.kovalyeva@gmail.com>
2025-01-02 18:55:46 +0300

    Added comments, redrew the graph for clarity

--- eda.py
+++ eda.py
@@ -3,10 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 
+# Функция для чтения CSV файла и возврата DataFrame
 def read_csv_file(filename):
     df = pd.read_csv(filename)
     return df
 
+# Функция для проверки и создания директории
 def check_dir(dir_name):
     try:
         if not os.path.exists(dir_name):
@@ -14,18 +16,21 @@ def check_dir(dir_name):
     except OSError as e:
         print(f"Error occurred while creating the directory: {e}")
 
+# Функция для построения и сохранения матрицы корреляции
 def plot_corr_matrix(df, title, dir_name):
     corr_matrix = df.corr()
 
     plt.figure(figsize=(8,6))
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f')
     plt.title(title)
+    plt.tight_layout()
 
     check_dir(dir_name)
     filename = os.path.join(dir_name, f'{title}.png')
     plt.savefig(filename, dpi=300, bbox_inches='tight')
     plt.show()
 
+# Функция для построения и сохранения гистограммы
 def plot_histogram(column, df, dir_name, bins=30, kde=True):
     plt.figure(figsize=(8, 6))
     sns.histplot(df[column], bins=bins, kde=kde, color='blue')
@@ -33,24 +38,28 @@ def plot_histogram(column, df, dir_name, bins=30, kde=True):
     plt.title(title)
     plt.xlabel(column)
     plt.ylabel('Frequency')
+    plt.tight_layout()
 
     check_dir(dir_name)
     filepath = os.path.join(dir_name, f'histogram_{column}.png')
     plt.savefig(filepath, dpi=300, bbox_inches='tight')
     plt.show()
 
+# Функция для построения и сохранения boxplot
 def plot_boxplot(column, df, dir_name):
     plt.figure(figsize=(8, 6))
     sns.boxplot(y=df[column], color='blue')
     title = f'Boxplot for {column}'
     plt.title(title)
     plt.ylabel(column)
+    plt.tight_layout()
 
     check_dir(dir_name)
     filepath = os.path.join(dir_name, f'boxplot_{column}.png')
     plt.savefig(filepath, bbox_inches='tight')
     plt.show()
 
+# Функция для удаления дубликатов из DataFrame
 def drop_duplicates(df):
     cnt_duplicates = df.duplicated().sum()
     print(f"Number of duplicate rows: {cnt_duplicates}")
@@ -61,6 +70,7 @@ def drop_duplicates(df):
     else:
         return df
 
+# Функция для проведения анализа данных (EDA)
 def eda(df):
     print('Dataframe info:')
     print(df.info())
@@ -69,6 +79,7 @@ def eda(df):
     print('Column names of the dataset:')
     print(df.columns)
 
+    # Построение матрицы корреляции до обработки
     plot_corr_matrix(df, title='Correlation Matrix Before Preprocessing', dir_name='eda')
 
     #The correlation with target column is 0.99
@@ -76,19 +87,24 @@ def eda(df):
 
     df = drop_duplicates(df)
 
+    # Построение гистограмм и boxplot для каждого числового столбца
     for column in df.columns:
         plot_histogram(column, df, dir_name='eda')
         plot_boxplot(column, df, dir_name='eda')
 
+    # Построение матрицы корреляции после обработки
     plot_corr_matrix(df, title='Correlation Matrix After Preprocessing', dir_name='eda')
 
     return df
 
 
 if __name__ == "__main__":
+    # Загрузка данных
     filepath = 'data/house_price_regression_dataset.csv'
     df = read_csv_file(filepath)
 
+    # Проведение EDA
     cleaned_df = eda(df)
 
+    # Сохранение очищенных данных
     cleaned_df.to_csv('data/cleaned_dataset.csv', index=False)
--- train_test.py
+++ train_test.py
@@ -9,45 +9,64 @@ import numpy as np
 import json
 from eda import read_csv_file, check_dir
 
+# Функция для разделения данных на обучающую и тестовую выборки с предварительной обработкой
 def train_test(df):
     df = df.reset_index(drop=True)
-    df['House_Price'] = np.log1p(df['House_Price'])  # Логарифмирование
+    df['House_Price'] = np.log1p(df['House_Price'])  # Логарифмирование целевой переменной
     X = df.drop('House_Price', axis=1)
     y = df['House_Price']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101, shuffle=True)
 
+    # Масштабирование признаков
     scaler = MinMaxScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
     return X_train, X_test, y_train, y_test, X.columns.tolist()
 
+# Функция для построения графика "Фактические vs Предсказанные"
 def plot_actual(y_test, y_pred, model_name):
     plt.scatter(y_test, y_pred, alpha=0.5)
-    plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red',
-             linestyle='--')  # Линия идеального предсказания
+    plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')
     plt.xlabel('Actual')
     plt.ylabel('Predicted')
-    plt.title('Actual vs Predicted')
+    plt.title(f'Actual vs Predicted for {model_name}')
     filename = f"results/actual_vs_predicted_{model_name}.png"
     plt.savefig(filename)
     plt.show()
 
+# Функция для построения графика остатков
 def plot_tails(y_test, y_pred, model_name):
     residuals = y_test - y_pred
     plt.scatter(y_pred, residuals, alpha=0.5)
     plt.axhline(y=0, color='red', linestyle='--')  # Горизонтальная линия на уровне 0
     plt.xlabel('Predicted')
     plt.ylabel('Residuals')
-    plt.title('Residual Plot')
+    plt.title(f'Residual Plot for {model_name}')
     filename = f"results/residual_plot_{model_name}.png"
+    plt.savefig(filename)
+    plt.show()
+
+# Построение важности признаков
+def plot_feature_importance(model, feature_names, model_name):
+    importances = model.feature_importances_
+
+    # Визуализация важности признаков
+    plt.barh(range(len(importances)), importances, align='center')
+    plt.yticks(range(len(importances)), feature_names)  # Используем список feature_names
+    plt.xlabel("Feature Importance")
+    plt.title(f"Feature Importance for {model_name}")
+    plt.tight_layout()
+    filename = f"results/feature_importance_{model_name}.png"
     plt.savefig(filename)
     plt.show()
 
 """Linear Regression"""
 def Linear_Regression_with_cv(X_train, X_test, y_train):
+    # Линейная регрессия с кросс-валидацией
     model = LinearRegression()
+    # Настраиваем кросс-валидацию с 5 фолдами
     kf = KFold(n_splits=5, shuffle=True, random_state=101)
     cv_scores = cross_val_score(model, X_train, y_train, cv=kf, scoring='neg_mean_squared_error')
     mse_scores = -cv_scores
@@ -57,28 +76,17 @@ def Linear_Regression_with_cv(X_train, X_test, y_train):
     print(f'Mean MSE (Cross-Validation): {mse_scores.mean()}')
     print(f'Standard Deviation of MSE (Cross-Validation): {mse_scores.std()}')
 
+    # Обучаем модель на полной обучающей выборке
     model.fit(X_train, y_train)
-
+    # Делаем предсказания на тестовой выборке
     y_pred = model.predict(X_test)
 
     return y_pred
 
-def plot_feature_importance(model, feature_names, model_name):
-    importances = model.feature_importances_
-
-    # Визуализация важности признаков
-    plt.barh(range(len(importances)), importances, align='center')
-    plt.yticks(range(len(importances)), feature_names)  # Используем список feature_names
-    plt.xlabel("Feature Importance")
-    plt.title(f"Feature Importance for {model_name}")
-    filename = f"results/feature_importance_{model_name}.png"
-    plt.savefig(filename)
-    plt.show()
-
 """XGBoost"""
 def XGBoost_with_cv(X_train, X_test, y_train, feature_names):
     model = xgb.XGBRegressor(objective='reg:squarederror', seed=101)
-
+    # Задаем сетку гиперпараметров
     param_grid = {
         'max_depth': [3, 6, 9],
         'learning_rate': [0.01, 0.1, 0.2],
@@ -86,32 +94,35 @@ def XGBoost_with_cv(X_train, X_test, y_train, feature_names):
         'subsample': [0.8, 1.0],
         'colsample_bytree': [0.8, 1.0]
     }
-
+    # Выполняем GridSearchCV для поиска лучших параметров
     grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='neg_mean_squared_error',
                                cv=5, n_jobs=-1, verbose=1)
     grid_search.fit(X_train, y_train)
     best_model = grid_search.best_estimator_
 
     y_pred = best_model.predict(X_test)
+    # Строим график важности признаков
     plot_feature_importance(best_model, feature_names, 'XGBoost')
     return y_pred
 
 """Random Forest"""
 def RandomForest_with_cv(X_train, X_test, y_train, feature_names):
     model = RandomForestRegressor(random_state=101)
+    # Сетка гиперпараметров
     param_grid = {
         'n_estimators': [100, 200, 300],
         'max_depth': [None, 10, 20, 30],
         'min_samples_split': [2, 5, 10],
         'min_samples_leaf': [1, 2, 4]
     }
-
+    # GridSearchCV для выбора оптимальных параметров
     grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='neg_mean_squared_error',
                                cv=5, n_jobs=-1, verbose=1)
     grid_search.fit(X_train, y_train)
     best_model = grid_search.best_estimator_
 
     y_pred = best_model.predict(X_test)
+    # Строим график важности признаков
     plot_feature_importance(best_model, feature_names, 'Random Forest')
     return y_pred
 
@@ -156,11 +167,14 @@ def manual_metrics(y_test, y_pred):
 
 
 if __name__ == "__main__":
+    # Загрузка данных
     filepath = 'data/cleaned_dataset.csv'
     df = read_csv_file(filepath)
 
+    # Разделение данных на тестовую и обучающую выборки
     X_train, X_test, y_train, y_test, feature_names = train_test(df)
 
+    # Проверка существования директории
     check_dir('results')
 
     data = {}
@@ -171,6 +185,7 @@ if __name__ == "__main__":
     y_pred = np.expm1(y_pred)
     y_test = np.expm1(y_test)
 
+    # Сохранение метрик для текущей модели
     data[model_name] = {
             'sklearn':
                 sklearn_metrics(y_test, y_pred),
@@ -186,13 +201,13 @@ if __name__ == "__main__":
     y_pred = RandomForest_with_cv(X_train, X_test, y_train, feature_names)
     y_pred = np.expm1(y_pred)
 
+    # Сохранение метрик для текущей модели
     data[model_name] = {
             'sklearn':
                 sklearn_metrics(y_test, y_pred),
             'manual':
                 manual_metrics(y_test, y_pred)
     }
-
 
     plot_actual(y_test, y_pred, model_name)
     plot_tails(y_test, y_pred, model_name)
@@ -202,6 +217,7 @@ if __name__ == "__main__":
     y_pred = XGBoost_with_cv(X_train, X_test, y_train, feature_names)
     y_pred = np.expm1(y_pred)
 
+    # Сохранение метрик для текущей модели
     data[model_name] = {
             'sklearn':
                 sklearn_metrics(y_test, y_pred),
